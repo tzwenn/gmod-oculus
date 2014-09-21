927ed674fb36428e1170074c2f9ff9ab0b79caa2
Sven Koehler <sven.koehler@student.hpi.uni-potsdam.de>
2014-09-21 15:28:08 +0200

    Stupid, stupid bug it was. Anyway. Roll broken. I could not care less

--- cpp/EulerAngles.h
+++ cpp/EulerAngles.h
@@ -17,13 +17,24 @@ struct EulerAngles
 		using namespace OVR;
 		static const double scale = 180.0 / M_PI;
 
-		Quatd quat(q.w, q.x, q.y, q.z);
+		Quatd quat(q.x, q.y, q.z, q.w);
 
 		//quat.GetEulerAngles<Axis_Y, Axis_X, Axis_Z, Rotate_CW, Handed_L>(&yaw, &pitch, &roll);
-		quat.GetEulerAngles<Axis_Y, Axis_Z, Axis_X, Rotate_CCW, Handed_L>(&yaw, &pitch, &roll);
+		quat.GetEulerAngles<Axis_Y, Axis_Z, Axis_X, Rotate_CCW, Handed_L>(&yaw, &roll, &pitch);
 		pitch *= scale;
-		yaw *= scale;
-		roll *= scale;
-		roll -= 180;
+		yaw *= scale * -1;
+		roll *= scale * -1;
+
+
+		corr(pitch);
+		corr(yaw);
+		corr(roll);
+	}
+
+private:
+
+	void corr(double & a)
+	{
+		a = a < -180 ? a + 360 : a;
 	}
 };
--- lua/oculusview.lua
+++ lua/oculusview.lua
@@ -5,9 +5,7 @@ local function OculusView( ply, pos, angles, fov )
 
 	view.origin = pos
 	pitch, yaw, roll = OculusGetRotation()
-	--view.angles = Angle(180 - rz, ry + angles.y - 180, -rx)
-	--view.angles = Angle(180 + rz, 180 + angles.y - rx, ry)
-	view.angles = Angle(pitch, yaw, roll)
+	view.angles = Angle(pitch, yaw + angles.y, roll * 0)
 	view.fov = fov
 
 	return view
@@ -17,12 +15,12 @@ end
 function StartOcculus()
 	OculusResetSensor()
 	hook.Add("CalcView", "OculusView", OculusView)
---	manually_pp_stereoscpy = false
+	manually_pp_stereoscpy = true
 end
 
 function StopOcculus()
 	hook.Remove("CalcView", "OculusView")
---	manually_pp_stereoscpy = false
+	manually_pp_stereoscpy = false
 end
 
 StartOcculus()
